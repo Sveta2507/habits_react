56375ddf6443c32759b85e29045134a4252f7e83
Sveta2507 <dnsdns.dn@googlemail.com>
2020-11-28 11:21:08 +0200

    commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
+// import { computeHeadingLevel } from "@testing-library/react";
 import "./App.css";
+import Auth from "./components/Auth/Auth.js";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <h1>HABITS</h1>
+      <Auth />
+    </>
+  );
 }
 
 export default App;
---
+++ src/components/Auth/Auth.js
@@ -0,0 +1,12 @@
+import React from "react";
+const Auth = () => {
+  return (
+    <>
+      <img src="../images/ЛОГО.png" alt="logo" />
+      <button>вход</button>
+      <button>регистрация</button>
+    </>
+  );
+};
+
+export default Auth;
